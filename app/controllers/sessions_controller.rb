c31e900001a7faf2876b12e271026c3430c07267
Pedula <renato.pedula@sr2.uerj.br>
2015-05-05 17:09:00 -0300

    melhorias no sistema de login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	@user = User.find_by(email: params[:session][:email].downcase) 
+  	@user = User.find_by(email:[:email]) 
   	if @user && user.authenticate(params[:session][:password])
   		 sign_in @user
   	else
